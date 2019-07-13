346e8748d14f49d6f8a127616cbd7e7d3df8a916
Vedvardhan <vedvardhan.g@gmail.com>
2019-07-13 11:15:55 +0530

    changed route to'/numbers'

--- node.js
+++ node.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(req,res){
  if (req.url==='/') {
    res.end("<h1>Hello World</h>")
  }
- if (req.url==='/about') {
+ if (req.url==='/numbers') {
 
    res.end("<html><script>var i=0;while (i<100){i++;document.write(i);}</script></html>");
  }
