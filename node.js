59639de3d7c5dc0ad78151f1e08d3e468c231bc8
Vedvardhan <vedvardhan.g@gmail.com>
2019-07-13 11:09:40 +0530

    added node.js

---
+++ node.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+var server = http.createServer(function(req,res){
+
+ if (req.url==='/') {
+   res.end("<h1>Hello World</h>")
+ }
+ if (req.url==='/about') {
+
+   res.end("<html><script>var i=0;while (i<100){i++;document.write(i);}</script></html>");
+ }
+
+
+}).listen(9000,console.log('server running...'))
